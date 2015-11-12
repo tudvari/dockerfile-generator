f7de2f1ee6fc7adda90867b3979cce3b6dd5aaf4
Tibor Udvari <tudvari@tudvari.com>
2015-11-12 13:09:05 +0100

    test coverage increased,copy,run element covered

--- tests.js
+++ tests.js
@@ -16,7 +16,7 @@ describe('dockerfile-generator', function() {
   }) ;
   it('Full test with all element ok', function(done) {
     let expected = fs.readFileSync('./tests/all_element_test.out');
-    generator.generate(fs.readFileSync('./tests/all_element_test_input.json'), function(err, result) {      
+    generator.generate(fs.readFileSync('./tests/all_element_test_input.json'), function(err, result) {
       should.not.exist(err) ;
       should.equal(result,expected.toString()) ;
     });
@@ -40,6 +40,20 @@ describe('dockerfile-generator', function() {
     generator.generate(fs.readFileSync('./tests/5_parsable_input_imageinfos_missing.json'), function(err, result) {
       should.exist(err) ;
       should.equal(err.message, 'Input JSON has a semantic error! (imagename or imageversion)') ;
+    });
+    done() ;
+  }) ;
+  it('Copy element missing', function(done) {
+    generator.generate(fs.readFileSync('./tests/6_parsable_input_copy_missing.json'), function(err, result) {
+      should.exist(err) ;
+      should.equal(err.message, 'Input JSON has a semantic error! (copy)') ;
+    });
+    done() ;
+  }) ;
+  it('RUN element missing', function(done) {
+    generator.generate(fs.readFileSync('./tests/7_parsable_input_run_missing.json'), function(err, result) {
+      should.exist(err) ;
+      should.equal(err.message, 'Input JSON has a semantic error! (run)') ;
     });
     done() ;
   }) ;

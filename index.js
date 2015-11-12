e1201de01b3158d42d736dc904ff9ca21d5898f0
Tibor Udvari <tudvari@tudvari.com>
2015-11-12 12:33:44 +0100

    copy and run keyword support

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict' ;
 
 module.exports.generate = function(input, cb) {
-  let incomingJSON;
+  let incomingJSON = undefined;
   let result = '';
   try {
     incomingJSON = JSON.parse(input) ;
@@ -10,9 +10,42 @@ module.exports.generate = function(input, cb) {
   }
   //processing imageName and imageVersion
   if (!incomingJSON['imagename'] || !incomingJSON['imageversion']) {
-    return cb(new Error('Input JSON has a semantic error!'));
+    return cb(new Error('Input JSON has a semantic error! (imagename or imageversion)'));
   } else {
-    result = 'FROM ' + incomingJSON['imagename'] + ':' + incomingJSON['imageversion'];
+    result = 'FROM ' + incomingJSON['imagename'] + ':' + incomingJSON['imageversion']+'\n';
+  }
+  //processing copy commands
+  if(!incomingJSON['copy'] || !Array.isArray(incomingJSON['copy'])){
+    return cb(new Error('Input JSON has a semantic error! (copy)'));
+  }
+  else{
+    let cpyArray = incomingJSON['copy'] ;
+    for(let cpyArrayElement of cpyArray){
+      result = result + 'COPY '+cpyArrayElement.src+' '+cpyArrayElement.dst+'\n';
+    }
+  }
+  //processing run commands
+  if(!incomingJSON['run'] || !Array.isArray(incomingJSON['run'])){
+    return cb(new Error('Input JSON has a semantic error! (run)'));
+  }
+  else{
+    let runArray = incomingJSON['run'] ;
+    for(let runArrayElement of runArray){
+      result = result + 'RUN [\"'+runArrayElement.command ;
+      let args = runArrayElement.args ;
+      if(!args.length){
+        result = result +'"]\n' ;
+      }
+      else{
+        result = result + '",' ;
+        for(let argItem of args){
+          result = result + '"'+argItem+'"' ;
+          result = result+',' ;
+        }
+        result = result.slice(0,-1);
+        result = result + ']\n' ;
+      }
+    }
   }
   return cb(null, result);
 }
--- tests.js
+++ tests.js
@@ -14,19 +14,22 @@ describe('dockerfile-generator', function() {
     });
     done() ;
   }) ;
-  it('ImageName and imageVersion exist', function(done) {
-    generator.generate(fs.readFileSync('./tests/2_parsable_input_from.json'), function(err, result) {
+  it('Full test with all element ok', function(done) {
+    let expected = fs.readFileSync('./tests/all_element_test.out');
+    generator.generate(fs.readFileSync('./tests/all_element_test_input.json'), function(err, result) {      
       should.not.exist(err) ;
-      should.equal(result, 'FROM node:4.1.2') ;
+      should.equal(result,expected.toString()) ;
     });
     done() ;
   }) ;
+
   it('ImageName does not exist', function(done) {
     generator.generate(fs.readFileSync('./tests/3_parsable_input_imagename_missing.json'), function(err, result) {
       should.exist(err) ;
     });
     done() ;
   }) ;
+
   it('ImageVersion does not exist', function(done) {
     generator.generate(fs.readFileSync('./tests/4_parsable_input_imageversion_missing.json'), function(err, result) {
       should.exist(err) ;
@@ -36,7 +39,7 @@ describe('dockerfile-generator', function() {
   it('ImageName and imageVersion are missing', function(done) {
     generator.generate(fs.readFileSync('./tests/5_parsable_input_imageinfos_missing.json'), function(err, result) {
       should.exist(err) ;
-      should.equal(err.message, 'Input JSON has a semantic error!') ;
+      should.equal(err.message, 'Input JSON has a semantic error! (imagename or imageversion)') ;
     });
     done() ;
   }) ;

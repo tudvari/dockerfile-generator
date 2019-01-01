bf1f156eaa5c7c4cc9a65fbf96dbc6e97704df75
Tibor Udvari <tudvari@tudvari.com>
2019-01-01 11:44:09 +0100

    label, env support

--- processorTests.js
+++ processorTests.js
@@ -26,4 +26,35 @@ describe('ProcessorTests', function() {
     it('RUN - array', function() {
         processor.processCmd(['test.cmd', '-b', 'testparam']).should.equal('CMD [ "test.cmd", "-b", "testparam" ]')
     })
+
+    it('LABEL - array', function() {
+        let params = []
+        params['key1'] = 'value1'
+        params['key2'] = 'value2'
+        processor.processLabels(params).should.equal('LABEL key1=value1\nLABEL key2=value2\n')
+    })
+
+    it('LABEL - object', function() {
+        let params = {
+            key1: 'value1',
+            key2: 'value2'
+        }
+        processor.processLabels(params).should.equal('LABEL key1=value1\nLABEL key2=value2\n')
+    })
+
+    it('ENV - array', function() {
+        let params = []
+        params['key1'] = 'value1'
+        params['key2'] = 'value2'
+        processor.processEnvs(params).should.equal('ENV key1=value1\nENV key2=value2\n')
+    })
+
+    it('ENV - object', function() {
+        let params = {
+            key1: 'value1',
+            key2: 'value2'
+        }
+        processor.processEnvs(params).should.equal('ENV key1=value1\nENV key2=value2\n')
+    })
+
 })

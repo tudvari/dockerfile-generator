cc47847ca5850a78b4cea20500fb5e03e9292051
Tibor Udvari <tudvari@tudvari.com>
2019-01-13 12:43:35 +0100

    json Generator v1

--- jsonGeneratorTests.js
+++ jsonGeneratorTests.js
@@ -18,13 +18,32 @@ describe('JSONGenerator Tests', function() {
         generateResult.should.eql({ from: 'nginx:latest' })
     })
 
-    it.skip('Valid JSON - FROM, ARG', function() {
-        let generateResult = jsonGenerator.generateDockerFile({ from: 'nginx:latest', args:["arg1", "arg2"] })
-        generateResult.should.equal('FROM nginx:latest\nARG arg1\nARG arg2\n')
+    it('Valid JSON - FROM, CMD Array', async function() {
+        
+        let buf = Buffer.from('FROM nginx:latest\nCMD [ "test.cmd", "-b" ]\nCMD [ "test2.cmd", "-b2" ]\n');
+        let bufferStream = new Stream.PassThrough();
+        bufferStream.end(buf);
+
+        let generateResult = await jsonGenerator.generateJSON(bufferStream)
+        generateResult.should.eql({from: 'nginx:latest', cmd: ["test.cmd", "-b", "test2.cmd", "-b2"]})
     })
 
-    it.skip('Valid JSON - FROM, CMD', function() {
-        let generateResult = jsonGenerator.generateDockerFile({ from: 'nginx:latest', cmd:["test.cmd", "-b"] })
-        generateResult.should.equal('FROM nginx:latest\nCMD [ "test.cmd", "-b" ]\n')
+    it('Valid JSON - FROM, ENV', async function() {
+        
+        let buf = Buffer.from('FROM nginx:latest\nENV env1=value1\nENV env2=value2\n');
+        let bufferStream = new Stream.PassThrough();
+        bufferStream.end(buf);
+
+        let generateResult = await jsonGenerator.generateJSON(bufferStream)
+        
+        let env = {}
+        env['env1'] = 'value1'
+        env['env2'] = 'value2'
+
+        let resp = {}
+        resp['from'] = 'nginx:latest'
+        resp['env'] = env
+
+        generateResult.should.eql(resp)
     })
 })

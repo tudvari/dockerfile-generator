6cdad0d434319c4600b2fbb8514a074b257002c6
Tibor Udvari <tudvari@tudvari.com>
2020-01-25 19:50:48 +0100

    code style change

--- Gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp')
-var eslint = require('gulp-eslint')
-
-function lint() {
-	return gulp.src(['index.js'])
-		.pipe(eslint())
-		.pipe(eslint.formatEach())
-}
-exports.default = lint

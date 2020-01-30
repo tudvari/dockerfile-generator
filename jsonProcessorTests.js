d5bccb5ecf7094aa15a5b76ddc02ddf8d0264a7e
Tibor Udvari <tibor.udvari@logmein.com>
2020-01-30 13:22:12 +0100

    Migrate airbnb code style

--- jsonProcessorTests.js
+++ jsonProcessorTests.js
@@ -1,275 +1,254 @@
-const path = require('path')
-
-const mocha = require('mocha')
-const should = require('should')
-
-const jsonProcessor = require(path.resolve(__dirname + '/lib/jsonProcessor'))
-
-describe('jsonProcessorTests - determineTests', function() {
-
-    it('determine - Single param', function(){
-        let foundFunction = jsonProcessor.determineFunction('FROM nginx:latest')
-        foundFunction.name.should.equal('processFROM')
-    })
-
-    it('determine - Single param', function(){
-        let foundFunction = jsonProcessor.determineFunction('EXPOSE 80/tcp')
-        foundFunction.name.should.equal('processEXPOSE')
-    })
-
-    it('determine - Single param', function(){
-        let foundFunction = jsonProcessor.determineFunction('VOLUME /home/testuser')
-        foundFunction.name.should.equal('processVOLUME')
-    })
-
-    it('determine - Single param', function(){
-        let foundFunction = jsonProcessor.determineFunction('USER testuser')
-        foundFunction.name.should.equal('processUSER')
-    })
-
-    it('determine - Single param', function(){
-        let foundFunction = jsonProcessor.determineFunction('WORKDIR /work')
-        foundFunction.name.should.equal('processWORKDIR')
-    })
-
-    it('determine - Single param', function(){
-        let foundFunction = jsonProcessor.determineFunction('STOPSIGNAL sigterm')
-        foundFunction.name.should.equal('processSTOPSIGNAL')
-    })
-
-    it('determine - mutliple params (array)', function(){
-        let resp = jsonProcessor.determineFunction('CMD ["test.cmd","-b","param"]')
-        resp.name.should.equal('processCMD')
-    })
-
-    it('determine - mutliple params (array)', function(){
-        let resp = jsonProcessor.determineFunction('RUN ["test.run","-b","param"]')
-        resp.name.should.equal('processRUN')
-    })
-
-    it('determine - mutliple params (array)', function(){
-        let resp = jsonProcessor.determineFunction('ENTRYPOINT ["test.run","-b","param"]')
-        resp.name.should.equal('processENTRYPOINT')
-    })
-
-    it('determine - mutliple params (array)', function(){
-        let resp = jsonProcessor.determineFunction('ARG arg1')
-        resp.name.should.equal('processARG')
-    })
-
-    it('determine - mutliple params (simple params)', function(){
-        let resp = jsonProcessor.determineFunction('COPY src dst')
-        resp.name.should.equal('processCOPY')
-    })
-
-    it('determine - key-value pair', function(){
-        let resp = jsonProcessor.determineFunction('LABEL key=value')
-        resp.name.should.equal('processLABEL')
-    })
-
-    it('determine - key-value pair', function(){
-        let resp = jsonProcessor.determineFunction('LABEL key=value')
-        resp.name.should.equal('processLABEL')
-    })
-
-    it('determine - key-value pair', function(){
-        let resp = jsonProcessor.determineFunction('ENV key=value')
-        resp.name.should.equal('processENV')
-    })
-})
-
-describe('jsonProcessorTests - processTests', function(){
-    it('process - FROM', function(){
-        let foundFunction = jsonProcessor.determineFunction('FROM nginx:latest')
-        foundFunction.name.should.equal('processFROM')
-        
-        // call the function
-        let respObject = foundFunction('FROM nginx:latest')
-
-        respObject.from.should.be.equal('nginx:latest')
-    })
-
-    it('process - EXPOSE', function(){
-        let foundFunction = jsonProcessor.determineFunction('EXPOSE 80/tcp')
-        foundFunction.name.should.equal('processEXPOSE')
-        
-        // call the function
-        let respObject = foundFunction('EXPOSE 80/tcp')
-
-        respObject.expose.should.be.equal('80/tcp')
-    })
-
-    it('process - VOLUME', function(){
-        let foundFunction = jsonProcessor.determineFunction('VOLUME /home/testvolume')
-        foundFunction.name.should.equal('processVOLUME')
-        
-        // call the function
-        let respObject = foundFunction('VOLUME /home/testvolume')
-
-        respObject.volume.should.be.equal('/home/testvolume')
-    })
-
-    it('process - USER', function(){
-        let foundFunction = jsonProcessor.determineFunction('USER testuser')
-        foundFunction.name.should.equal('processUSER')
-        
-        // call the function
-        let respObject = foundFunction('USER testuser')
-
-        respObject.user.should.be.equal('testuser')
-    })
-
-    it('process - WORKDIR', function(){
-        let foundFunction = jsonProcessor.determineFunction('WORKDIR /work')
-        foundFunction.name.should.equal('processWORKDIR')
-        
-        // call the function
-        let respObject = foundFunction('WORKDIR /work')
-
-        respObject.workdir.should.be.equal('/work')
-    })
-
-    it('process - STOPSIGNAL', function(){
-        let foundFunction = jsonProcessor.determineFunction('STOPSIGNAL sigterm')
-        foundFunction.name.should.equal('processSTOPSIGNAL')
-        
-        // call the function
-        let respObject = foundFunction('STOPSIGNAL sigterm')
+const path = require('path');
+
+const jsonProcessor = require(path.resolve(__dirname + '/lib/jsonProcessor'));
+
+describe('jsonProcessorTests - determineTests', () => {
+  it('determine - Single param', () => {
+    const foundFunction = jsonProcessor.determineFunction('FROM nginx:latest');
+    foundFunction.name.should.equal('processFROM');
+  });
+
+  it('determine - Single param', () => {
+    const foundFunction = jsonProcessor.determineFunction('EXPOSE 80/tcp');
+    foundFunction.name.should.equal('processEXPOSE');
+  });
+
+  it('determine - Single param', () => {
+    const foundFunction = jsonProcessor.determineFunction('VOLUME /home/testuser');
+    foundFunction.name.should.equal('processVOLUME');
+  });
 
-        respObject.stopsignal.should.be.equal('sigterm')
-    })
+  it('determine - Single param', () => {
+    const foundFunction = jsonProcessor.determineFunction('USER testuser');
+    foundFunction.name.should.equal('processUSER');
+  });
 
-    it('process - ARG', function(){
-        let foundFunction = jsonProcessor.determineFunction('ARG arg1')
-        foundFunction.name.should.equal('processARG')
-        
-        // call the function
-        let respObject = foundFunction('ARG arg1')
+  it('determine - Single param', () => {
+    const foundFunction = jsonProcessor.determineFunction('WORKDIR /work');
+    foundFunction.name.should.equal('processWORKDIR');
+  });
 
-        respObject.arg.should.be.equal('arg1')
-    })
+  it('determine - Single param', () => {
+    const foundFunction = jsonProcessor.determineFunction('STOPSIGNAL sigterm');
+    foundFunction.name.should.equal('processSTOPSIGNAL');
+  });
 
-    it('process - CMD', function(){
-        let foundFunction = jsonProcessor.determineFunction('CMD ["test.cmd","-b","param"]')
-        foundFunction.name.should.equal('processCMD')
+  it('determine - mutliple params (array)', () => {
+    const resp = jsonProcessor.determineFunction('CMD ["test.cmd","-b","param"]');
+    resp.name.should.equal('processCMD');
+  });
 
-        //call the function
-        let respObject = foundFunction('CMD ["test.cmd","-b","param"]')
+  it('determine - mutliple params (array)', () => {
+     const resp = jsonProcessor.determineFunction('RUN ["test.run","-b","param"]');
+     resp.name.should.equal('processRUN');
+  });
 
-        let expectedArray = new Array()
-        expectedArray.push("test.cmd")
-        expectedArray.push("-b")
-        expectedArray.push("param")
+  it('determine - mutliple params (array)', () => {
+    const resp = jsonProcessor.determineFunction('ENTRYPOINT ["test.run","-b","param"]');
+    resp.name.should.equal('processENTRYPOINT');
+  });
 
-        respObject.cmd.should.be.eql(expectedArray)
-    })
+  it('determine - mutliple params (array)', () => {
+    const resp = jsonProcessor.determineFunction('ARG arg1');
+    resp.name.should.equal('processARG');
+  });
 
-    it('process - RUN', function(){
-        let foundFunction = jsonProcessor.determineFunction('RUN ["test.run","-b","param"]')
-        foundFunction.name.should.equal('processRUN')
+  it('determine - mutliple params (simple params)', () => {
+    const resp = jsonProcessor.determineFunction('COPY src dst');
+    resp.name.should.equal('processCOPY');
+  });
 
-        //call the function
-        let respObject = foundFunction('RUN ["test.run","-b","param"]')
+  it('determine - key-value pair', () => {
+    const resp = jsonProcessor.determineFunction('LABEL key=value');
+    resp.name.should.equal('processLABEL');
+  });
 
-        let expectedArray = new Array()
-        expectedArray.push("test.run")
-        expectedArray.push("-b")
-        expectedArray.push("param")
+  it('determine - key-value pair', () => {
+    const resp = jsonProcessor.determineFunction('LABEL key=value');
+    resp.name.should.equal('processLABEL');
+  });
 
-        respObject.run.should.be.eql(expectedArray)
-    })
+  it('determine - key-value pair', () => {
+    const resp = jsonProcessor.determineFunction('ENV key=value');
+    resp.name.should.equal('processENV');
+  });
+});
 
-    it('process - SHELL', function(){
-        let foundFunction = jsonProcessor.determineFunction('SHELL ["test.sh","-b","param"]')
-        foundFunction.name.should.equal('processSHELL')
+describe('jsonProcessorTests - processTests', () => {
+  it('process - FROM', () => {
+    const foundFunction = jsonProcessor.determineFunction('FROM nginx:latest');
+    foundFunction.name.should.equal('processFROM');
 
-        //call the function
-        let respObject = foundFunction('SHELL ["test.sh","-b","param"]')
+    const respObject = foundFunction('FROM nginx:latest');
 
-        let expectedArray = new Array()
-        expectedArray.push("test.sh")
-        expectedArray.push("-b")
-        expectedArray.push("param")
+    respObject.from.should.be.equal('nginx:latest');
+  });
 
-        respObject.shell.should.be.eql(expectedArray)
-    })
+  it('process - EXPOSE', () => {
+    const foundFunction = jsonProcessor.determineFunction('EXPOSE 80/tcp');
+    foundFunction.name.should.equal('processEXPOSE');
 
-    it('process - ENTRYPOINT', function(){
-        let foundFunction = jsonProcessor.determineFunction('ENTRYPOINT ["test.run","-b","param"]')
-        foundFunction.name.should.equal('processENTRYPOINT')
+    const respObject = foundFunction('EXPOSE 80/tcp');
 
-        //call the function
-        let respObject = foundFunction('ENTRYPOINT ["test.run","-b","param"]')
+    respObject.expose.should.be.equal('80/tcp');
+  });
 
-        let expectedArray = new Array()
-        expectedArray.push("test.run")
-        expectedArray.push("-b")
-        expectedArray.push("param")
+  it('process - VOLUME', () => {
+    const foundFunction = jsonProcessor.determineFunction('VOLUME /home/testvolume');
+    foundFunction.name.should.equal('processVOLUME');
 
-        respObject.entrypoint.should.be.eql(expectedArray)
-    })
+    const respObject = foundFunction('VOLUME /home/testvolume');
 
-    it('process - COPY', function(){
-        let foundFunction = jsonProcessor.determineFunction('COPY src dst')
-        foundFunction.name.should.equal('processCOPY')
+    respObject.volume.should.be.equal('/home/testvolume');
+  });
 
-        //call the function
-        let respObject = foundFunction('COPY src dst')
+  it('process - USER', () => {
+    const foundFunction = jsonProcessor.determineFunction('USER testuser');
+    foundFunction.name.should.equal('processUSER');
 
-        let expectedArray = {}
-        expectedArray['src'] = 'dst'
+    const respObject = foundFunction('USER testuser');
 
-        respObject.copy.should.be.eql(expectedArray)
-    })
+    respObject.user.should.be.equal('testuser');
+  });
 
-    it('process - ADD', function(){
-        let foundFunction = jsonProcessor.determineFunction('ADD src dst')
-        foundFunction.name.should.equal('processADD')
+  it('process - WORKDIR', () => {
+    const foundFunction = jsonProcessor.determineFunction('WORKDIR /work');
+    foundFunction.name.should.equal('processWORKDIR');
 
-        //call the function
-        let respObject = foundFunction('ADD src dst')
+    const respObject = foundFunction('WORKDIR /work');
 
-        let expectedArray = {}
-        expectedArray['src'] = 'dst'
+    respObject.workdir.should.be.equal('/work');
+  });
 
-        respObject.add.should.be.eql(expectedArray)
-    })
+  it('process - STOPSIGNAL', () => {
+    const foundFunction = jsonProcessor.determineFunction('STOPSIGNAL sigterm');
+    foundFunction.name.should.equal('processSTOPSIGNAL');
 
-    it('process - LABEL', function(){
-        let foundFunction = jsonProcessor.determineFunction('LABEL key=value')
-        foundFunction.name.should.equal('processLABEL')
+    const respObject = foundFunction('STOPSIGNAL sigterm');
 
-        //call the function
-        let respObject = foundFunction('LABEL key=value')
-       
-        let expectedArray = {}
-        expectedArray['key'] = 'value'
+    respObject.stopsignal.should.be.equal('sigterm');
+  });
 
-        respObject.label.should.be.eql(expectedArray)
-    })
+  it('process - ARG', () => {
+    const foundFunction = jsonProcessor.determineFunction('ARG arg1');
+    foundFunction.name.should.equal('processARG');
+ 
+    const respObject = foundFunction('ARG arg1');
 
-    it('process - ENV', function(){
-        let foundFunction = jsonProcessor.determineFunction('ENV key=value')
-        foundFunction.name.should.equal('processENV')
+    respObject.arg.should.be.equal('arg1');
+  });
 
-        //call the function
-        let respObject = foundFunction('ENV key=value')
+  it('process - CMD', () => {
+    const foundFunction = jsonProcessor.determineFunction('CMD ["test.cmd", "-b","param"]');
+    foundFunction.name.should.equal('processCMD');
 
-        let expectedArray = {}
-        expectedArray['key'] = 'value'
+    const respObject = foundFunction('CMD ["test.cmd", "-b", "param"]');
 
-        respObject.env.should.be.eql(expectedArray)
-    })
+    const expectedArray = [];
+    expectedArray.push('test.cmd');
+    expectedArray.push('-b');
+    expectedArray.push('param');
 
-    it('process - COMMENT', function(){
-        let foundFunction = jsonProcessor.determineFunction('# Some value')
-        foundFunction.name.should.equal('processCOMMENT')
+    respObject.cmd.should.be.eql(expectedArray);
+  });
 
-        //call the function
-        let respObject = foundFunction('# Some value')
+  it('process - RUN', () => {
+    const foundFunction = jsonProcessor.determineFunction('RUN ["test.run","-b","param"]');
+    foundFunction.name.should.equal('processRUN');
 
-        let expectedValue = 'Some value'
+    const respObject = foundFunction('RUN ["test.run","-b","param"]');
 
-        respObject[Object.keys(respObject)[0]].should.be.eql(expectedValue)
-    })
-})
+    const expectedArray = [];
+    expectedArray.push('test.run');
+    expectedArray.push('-b');
+    expectedArray.push('param');
+
+    respObject.run.should.be.eql(expectedArray);
+  });
+
+  it('process - SHELL', () => {
+    const foundFunction = jsonProcessor.determineFunction('SHELL ["test.sh","-b","param"]');
+    foundFunction.name.should.equal('processSHELL');
+
+    const respObject = foundFunction('SHELL ["test.sh","-b","param"]');
+
+    const expectedArray = [];
+    expectedArray.push('test.sh');
+    expectedArray.push('-b');
+    expectedArray.push('param');
+
+    respObject.shell.should.be.eql(expectedArray);
+  });
+
+  it('process - ENTRYPOINT', () => {
+    const foundFunction = jsonProcessor.determineFunction('ENTRYPOINT ["test.run","-b","param"]');
+    foundFunction.name.should.equal('processENTRYPOINT');
+
+    const respObject = foundFunction('ENTRYPOINT ["test.run","-b","param"]');
+
+    const expectedArray = [];
+    expectedArray.push('test.run');
+    expectedArray.push('-b');
+    expectedArray.push('param');
+
+    respObject.entrypoint.should.be.eql(expectedArray);
+  });
+
+  it('process - COPY', () => {
+    const foundFunction = jsonProcessor.determineFunction('COPY src dst');
+    foundFunction.name.should.equal('processCOPY');
+
+    const respObject = foundFunction('COPY src dst');
+
+    const expectedArray = {};
+    expectedArray.src = 'dst';
+
+    respObject.copy.should.be.eql(expectedArray);
+  });
+
+  it('process - ADD', () => {
+    const foundFunction = jsonProcessor.determineFunction('ADD src dst');
+    foundFunction.name.should.equal('processADD');
+
+    const respObject = foundFunction('ADD src dst');
+
+    const expectedArray = {};
+    expectedArray.src = 'dst';
+
+    respObject.add.should.be.eql(expectedArray);
+  });
+
+  it('process - LABEL', () => {
+    const foundFunction = jsonProcessor.determineFunction('LABEL key=value');
+    foundFunction.name.should.equal('processLABEL');
+
+    const respObject = foundFunction('LABEL key=value');
+
+    const expectedArray = {};
+    expectedArray.key = 'value';
+
+    respObject.label.should.be.eql(expectedArray);
+  });
+
+  it('process - ENV', () => {
+    const foundFunction = jsonProcessor.determineFunction('ENV key=value');
+    foundFunction.name.should.equal('processENV');
+
+    const respObject = foundFunction('ENV key=value');
+
+    const expectedArray = {};
+    expectedArray.key = 'value';
+
+    respObject.env.should.be.eql(expectedArray);
+  });
+
+  it('process - COMMENT', () => {
+    const foundFunction = jsonProcessor.determineFunction('# Some value');
+    foundFunction.name.should.equal('processCOMMENT');
+
+    const respObject = foundFunction('# Some value');
+    const expectedValue = 'Some value';
+
+    respObject[Object.keys(respObject)[0]].should.be.eql(expectedValue);
+  });
+});

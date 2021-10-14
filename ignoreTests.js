0b1380e68b847c1faadfac2317dc45785714e8f0
tudvari <tudvari@tudvari.com>
2021-10-14 09:51:12 +0200

    change ecmaversion to 2017, code style fixes

--- ignoreTests.js
+++ ignoreTests.js
@@ -1,21 +1,21 @@
-const should = require('should');
-const { describe, it } = require('mocha');
+const should = require('should')
+const { describe, it } = require('mocha')
 
-const generator = require('.');
+const generator = require('.')
 
 describe('.dockerignore generate tests', () => {
-  it('Empty Array', async () => {
-    const resp = await generator.generateIgnoreFile([]);
-    should.equal(resp, '');
-  });
+	it('Empty Array', async () => {
+		const resp = await generator.generateIgnoreFile([])
+		should.equal(resp, '')
+	})
 
-  it('One Item in the Array', async () => {
-    const resp = await generator.generateIgnoreFile(['node_modules']);
-    should.equal(resp, 'node_modules\n');
-  });
+	it('One Item in the Array', async () => {
+		const resp = await generator.generateIgnoreFile(['node_modules'])
+		should.equal(resp, 'node_modules\n')
+	})
 
-  it('Multiple Item in the Array', async () => {
-    const resp = await generator.generateIgnoreFile(['node_modules', '.git']);
-    should.equal(resp, 'node_modules\n.git\n');
-  });
-});
+	it('Multiple Item in the Array', async () => {
+		const resp = await generator.generateIgnoreFile(['node_modules', '.git'])
+		should.equal(resp, 'node_modules\n.git\n')
+	})
+})
